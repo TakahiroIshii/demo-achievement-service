a748862df9ea750cefe15b79025d830059808194
TakahiroIshii <takaishiijp@gmail.com>
2021-05-26 15:05:45 +0900

    refactor: add sqs create queue to test init

--- tests/init.ts
+++ tests/init.ts
@@ -12,14 +12,17 @@ import { BaseRepository } from '../src/repositories';
 import { Logger, LogLevel } from '../src/utils';
 import { Redis } from '../src/clients';
 import { container, Lifecycle } from 'tsyringe';
+import { Sqs } from '../src/clients/queues';
 
 const logger = new Logger({ logLevel: LogLevel.Debug });
 container.register(Logger, { useValue: logger });
 
 const redis = container.resolve(Redis);
+const sqs = container.resolve(Sqs);
 
 before(async () => {
   await redis.flushMyKeys();
+  await sqs.client.createQueue({ QueueName: 'achievementQueue' }).promise();
   const thisContainer: any = container;
   const instances = Array.from(thisContainer._registry._registryMap).flatMap(([Class, [option]]) => {
     if (option?.options?.lifecycle !== Lifecycle.Singleton) {
