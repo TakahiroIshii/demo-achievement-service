34f0ff4a3a473cfc59b640602996c016694f016f
TakahiroIshii <takaishiijp@gmail.com>
2021-09-07 18:19:40 +0900

    refactor: change default region for SQS

--- configs/index.ts
+++ configs/index.ts
@@ -1,7 +1,7 @@
 import { LogLevel } from '../src/utils';
 
 const localhost = process.env.DOCKER_LOCALHOST || 'localhost';
-const sqsRegion = process.env.SQS_REGION || 'localhost';
+const sqsRegion = process.env.SQS_REGION || 'us-east-1';
 const sqsEndPoint = process.env.SQS_ENDPOINT || `http://${localhost}:4566`;
 const sqsUrlPrefix = process.env.SQS_URL_PREFIX || `http://${localhost}:4566/000000000000/`;
 
